c2a9b2e7e12b38f01d892b77096935546596dd14
Albert Franz <albert.franz@hotmail.de>
2025-06-29 22:13:00 +0200

    Frontend Grundstuktur überarbeitet und Komponenten

--- Frontend-Client/src/pages/LoginPage.js
+++ Frontend-Client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import QuizAcademyLogo from '../assets/QuizAcademyLogoBLANK.png';
+import './LoginPage.css'; 
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
